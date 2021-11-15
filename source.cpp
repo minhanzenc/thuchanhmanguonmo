7a37ebf148749c5b849054e9b8848f1a6d56d5ba
minhanzenc <minhanzenc@gmail.com>
2021-11-15 17:16:40 +0700

    Da them hieu

--- source.cpp
+++ source.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 using namespace std;
 int tong(int a, int b);
-int hieu(int a, int b);
+int hieu(int a, int b)
+{
+	return a-b;
+}
 
 int main()
 {
@@ -13,6 +16,10 @@ int main()
 	cin >> b;
 	cout << "Chon phep toan (+,-) ";
 	cin >> phepToan;
+	if(phepToan=="-")
+	{
+		cout << "Hieu la:"<<hieu(a,b);
+	}
 	system("pause");
 	return 0;
 }
